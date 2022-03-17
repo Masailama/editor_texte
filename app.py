e8295322ba23e778f9d3cc9e38fecbc20f21c347
Didac Gomez <didac.programacio@gmail.com>
2022-03-17 20:41:01 +0100

    funcionalitat dels botons y shortcuts afegir y  obrir, afegir icones de guardar

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from editor import Editor
 from file_list import FileList
 
 
@@ -14,23 +13,30 @@ class App(tk.Tk):
             self.attributes("-fullscreen", True)
             self.bind("<F11>", lambda _: self.attributes("-fullscreen", not self.attributes("-fullscreen")))
         self.file_list = FileList(self)
-        self.editor = Editor(self)
         self.bind_functions()
 
     def bind_functions(self):
         self.bind("<Control-q>", lambda _: self.destroy())
-        self.bind("<Control-n>", lambda _:self.add_file())
-        self.bind("<Control-o>", lambda _:self.open_file())
-        self.bind("<Control-r>", lambda _:self.run_file())
+        self.bind("<Control-n>", lambda _: self.add_file())
+        self.bind("<Control-s>", lambda _: self.save_file())
+        self.bind("<Control-Shift-S>", lambda _: self.save_as_file())
+        self.bind("<Control-o>", lambda _: self.open_file())
+        self.bind("<Control-r>", lambda _: self.run_file())
     
     def add_file(self):
         self.file_list.add_file()
     
     def open_file(self):
         self.file_list.open_file()
+
+    def save_file(self):
+        self.file_list.save_file()
     
+    def save_as_file(self):
+        self.file_list.save_as_file()
+
     def run_file(self):
-        pass
+        self.file_list.run_file()
 
 
 if __name__ == "__main__":
--- editor.py
+++ editor.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
-from utils import BACKGROUND
+from utils import BACKGROUND, FOREGROUND
 
 
 class Editor(tk.Frame):
     def __init__(self, window):
         tk.Frame.__init__(self, window, bg=BACKGROUND)
-        self.editor = tk.Text(self, height=32, bg=BACKGROUND, bd=0, relief="flat", highlightthickness=0)
-        self.terminal = tk.Text(self, bg=BACKGROUND, bd=0, relief="flat", highlightthickness=0)
+        self.editor = tk.Text(self, height=32, bg=BACKGROUND, bd=0, relief="flat", fg=FOREGROUND, 
+            highlightthickness=0, insertbackground="white")
+        self.terminal = tk.Text(self, bg=BACKGROUND, bd=0, relief="flat", fg=FOREGROUND, highlightthickness=0)
         self.draw()
 
     def draw(self):
@@ -15,3 +16,12 @@ class Editor(tk.Frame):
         self.editor.pack(fill=tk.X)
         ttk.Separator(self, orient=tk.HORIZONTAL).pack(fill=tk.X)
         self.terminal.pack(fill=tk.BOTH)
+
+    def run_file(self):
+        print("run file")
+
+    def load_file(self, path_file):
+        self.editor.delete(1.0, tk.END)
+        f = open(path_file)
+        self.editor.insert(1.0, f.read())
+        f.close()
